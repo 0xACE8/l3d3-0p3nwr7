e8316d15ad9bcd56191a3095d2ea70e174da9117
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-09-16 03:22:32 -0700

    Create d1y-p4r71.sh

---
+++ 7hund3r_71m3cl0ud/d1y-p4r71.sh
@@ -0,0 +1,3 @@
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git ace https://github.com/0xACE8/ace-packages' feeds.conf.default
+sed -i '$a src-git netspeedtest https://github.com/sirpdboy/netspeedtest' feeds.conf.default
