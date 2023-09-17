e9ada01bc7821ad731a2091ab8a0df2abf7105cb
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-09-17 04:25:25 -0700

    Update d1y-p4r71.sh

--- 7hund3r_71m3cl0ud/d1y-p4r71.sh
+++ 7hund3r_71m3cl0ud/d1y-p4r71.sh
@@ -1,3 +1,4 @@
 sed -i '$a src-git ace https://github.com/0xACE8/4c3-p4ck463' feeds.conf.default
-#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-#sed -i '$a src-git netspeedtest https://github.com/sirpdboy/netspeedtest' feeds.conf.default
+sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' feeds.conf.default
