3a5897f303f31961951956fb6bc18f1cfe94b329
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-09-16 20:56:41 -0700

    Update d1y-p4r71.sh

--- 7hund3r_71m3cl0ud/d1y-p4r71.sh
+++ 7hund3r_71m3cl0ud/d1y-p4r71.sh
@@ -1,3 +1,3 @@
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git ace https://github.com/0xACE8/4c3-p4ck463' feeds.conf.default
-sed -i '$a src-git netspeedtest https://github.com/sirpdboy/netspeedtest' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git netspeedtest https://github.com/sirpdboy/netspeedtest' feeds.conf.default
